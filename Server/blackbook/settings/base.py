d7a748e647e4c4e4a95fae972cbeb9e033269201
Miller Hooks <miller@DaffyFuck.hq.mutualmobile.com>
2011-05-02 11:28:05 -0500

    changed how the settings work. Added gitignore

---
+++ Server/blackbook/settings/base.py
@@ -0,0 +1,40 @@
+# Django settings for blackbook project.
+
+ADMINS = (
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
+# although not all choices may be available on all operating systems.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Chicago'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGE_CODE = 'en-us'
+
+USE_I18N = False
+
+# Make this unique, and don't share it with anybody.
+SECRET_KEY = '=b=q@^59aqyt-yu8%__fi*i_^^%!-nks9(gvrf#*ku9y*6(1-r'
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+
---
+++ Server/blackbook/settings/defaults.py
@@ -0,0 +1,66 @@
+import os
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here('..')
+
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+PROJECT_MODULE = '.'.join(__name__.split('.')[:-2])  # cut .settings.base
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+MEDIA_ROOT = root('static', 'uploads')
+MEDIA_URL = '/static/uploads/'
+
+STATIC_ROOT = root('static', 'assets')
+STATIC_URL = '/static/assets/'
+
+ADMIN_MEDIA_PREFIX = '/static/assets/admin/'
+
+STATICFILES_DIRS = (
+    root('staticfiles'),
+)
+
+ROOT_URLCONF = '%s.settings.urls' % PROJECT_MODULE
+
+TEMPLATE_DIRS = (
+    root('templates'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'django.contrib.gis',
+
+    # external
+    #'south',
+    'piston',
+
+    # internal
+    'iconography',
+)
+
+SITE_ID = 1
+
+TEST_EXCLUDE = ('django',)
+TEST_RUNNER = '%s.settings.test_suite.AdvancedTestSuiteRunner' % PROJECT_MODULE
+
+
